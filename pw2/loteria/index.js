3587e552a6a5cf6822919175e3596ef16316ee3f
BETHA\guilherme.bona <guilherme.bona@betha.com.br>
2022-03-07 15:57:26 -0300

    checkpoint

---
+++ pw2/loteria/index.js
@@ -0,0 +1,33 @@
+function generate(size, lowest, highest) {
+  
+  var highest= 60;
+  var size =6;
+	var numbers = [];
+	for(var i = 0; i < size; i++) {
+		var add = true;
+		var randomNumber = Math.floor(Math.random() * highest) + 1;
+		for(var y = 0; y < highest; y++) {
+			if(numbers[y] == randomNumber) {
+				add = false;
+			}
+		}
+		if(add) {
+			numbers.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+  
+	var highestNumber = 0;
+	for(var m = 0; m < numbers.length; m++) {
+		for(var n = m + 1; n < numbers.length; n++) {
+			if(numbers[n] < numbers[m]) {
+				highestNumber = numbers[m];
+				numbers[m] = numbers[n];
+				numbers[n] = highestNumber;
+			}
+		}
+	}
+  
+	document.getElementById("numbers").innerHTML = numbers.join(" - ");
+}
