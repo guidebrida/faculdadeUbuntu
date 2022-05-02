b323ab2a58a298d0a9aaa9dcfe7254880029e232
BETHA\guilherme.bona <guilherme.bona@betha.com.br>
2022-05-02 20:37:28 -0300

    Rotas + salvar em json

--- trabalho/login/cadastrar/cadastrar.js
+++ trabalho/login/cadastrar/cadastrar.js
@@ -2,3 +2,17 @@ function cadastrado(){
     alert("Cadastro do super heroi efetuado")
     window.location.href="../../index.html"
 }
+
+function guardaFormulario(){
+    var listaHeroi = new Object();
+    listaHeroi.nome = getElementById('nome');
+    listaHeroi.Inteligencia = getElementById('Inteligencia');
+    listaHeroi.Forca = getElementById('Forca');
+    listaHeroi.Velocidade = getElementById('Velocidade');
+    listaHeroi.Durabilidade = getElementById('Durabilidade');
+    listaHeroi.Poder = getElementById('Poder');
+    listaHeroi.Combate = getElementById('Combate');
+
+    console.log(listaHeroi.valueOf());
+
+}
